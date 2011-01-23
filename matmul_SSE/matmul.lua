5d05229ca978c0a9da3e2630b837f5f92f201902
Heng Li <lh3@live.co.uk>
2011-01-23 18:21:05 -0500

    move related functions to matlua.lua

--- matmul_SSE/matmul.lua
+++ matmul_SSE/matmul.lua
@@ -1,8 +1,30 @@
-require("klua")
+matrix = {}
 
+function matrix.T(a)
+	local m, n, x = #a, #a[1], {};
+	for i = 1, n do
+		x[i] = {};
+		for j = 1, m do x[i][j] = a[j][i] end
+	end
+	return x;
+end
+
+function matrix.mul(a, b)
+	assert(#a[1] == #b);
+	local m, n, p, x = #a, #a[1], #b[1], {};
+	local c = matrix.T(b); -- transpose for efficiency
+	for i = 1, m do
+		x[i] = {}
+		local xi = x[i];
+		for j = 1, p do
+			local sum, ai, cj = 0, a[i], c[j];
+			for k = 1, n do sum = sum + ai[k] * cj[k] end
+			xi[j] = sum;
+		end
+	end
+	return x;
+end
 local n = arg[1] or 100;
-
--- generate matrix
 
 function matgen()
 	local a = {}
@@ -15,7 +37,4 @@ function matgen()
 	return a;
 end
 
-local a = matgen();
-local b = matgen();
-
-local c = matrix.mul(a, b);
+local a = matrix.mul(matgen(), matgen());
