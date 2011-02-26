e11f3cc06ba3aff04fb89a7763f6773064169576
Heng Li <lh3@live.co.uk>
2011-02-25 20:49:07 -0500

    update bedutils.lua

--- seq/bedutils.lua
+++ seq/bedutils.lua
@@ -24,14 +24,19 @@ function io.xopen(fn, mode)
 	mode = mode or 'r';
 	if fn == nil then return io.stdin;
 	elseif fn == '-' then return (mode == 'r' and io.stdin) or io.stdout;
-	elseif fn:sub(-3) == '.gz' then return io.popen('gzip -dc ' .. fn, mode);
-	elseif fn:sub(-4) == '.bz2' then return io.popen('bzip2 -dc ' .. fn, mode);
+	elseif fn:sub(-3) == '.gz' then return (mode == 'r' and io.popen('gzip -dc ' .. fn, 'r')) or io.popen('gzip > ' .. fn, 'w');
+	elseif fn:sub(-4) == '.bz2' then return (mode == 'r' and io.popen('bzip2 -dc ' .. fn, 'r')) or io.popen('bgzip2 > ' .. fn, 'w');
 	else return io.open(fn, mode) end
 end
 --
 
-function bed_union(fp)
+--[[
+	BED utilities
+]]--
+
+function bed_merge(fn)
 	local c, b, e, n;
+	local fp = io.xopen(fn);
 	for l in fp:lines() do
 		local t = l:split('\t', 4);
 		t[2], t[3] = tonumber(t[2]), tonumber(t[3]);
@@ -44,10 +49,12 @@ function bed_union(fp)
 		end
 	end
 	print(c, b, e);
+	fp:close();
 end
 
-function bed_cov2(fp)
+function bed_cov2(fn)
 	local c, e, ob, oe; -- e is the rightmost pos;
+	local fp = io.xopen(fn);
 	ob, oe = -1, -1; -- ob and oe are the coordinates of the region to output
 	for l in fp:lines() do
 		local t = l:split('\t', 4);
@@ -68,18 +75,60 @@ function bed_cov2(fp)
 		end
 	end
 	if ob ~= -1 then print(c, ob, oe) end
+	fp:close();
+end
+
+--[[
+	PLIST utilities
+]]--
+
+function plist_read_list(fn)
+	local fp = io.xopen(fn2);
+	local hash = {};
+	for l in fp:lines() do
+		local chr, pos = l:match("^(%S+)%s+(%d+)");
+		hash[chr .. pos] = 1;
+	end
+	fp:close();
+	return hash;
+end
+
+function plist_uniq(fn1, fn2)
+	local fp, hash = io.xopen(fn1), plist_read_list(fn2);
+	for l in fp:lines() do
+		local chr, pos = l:match("^(%S+)%s+(%d+)");
+		if hash[chr .. pos] == nil then print(l) end
+	end
+	fp:close();
+end
+
+function plist_joint(fn1, fn2)
+	local fp, hash = io.xopen(fn1), plist_read_list(fn2);
+	for l in fp:lines() do
+		local chr, pos = l:match("^(%S+)%s+(%d+)");
+		if hash[chr .. pos] then print(l) end
+	end
+	fp:close();
 end
 
 -- main function
 
 if #arg == 0 then
-	print('Usage: bedtk.lua union|cov2 [in.bed]');
+	print('');
+	print('Usage:   bedutils.lua <command> <arguments>\n');
+	print('Command: merge     merge overlapping regions in a sorted BED');
+	print('         cov2      extract regions covered by >=2 records in a sorted BED');
+	print('');
+	print('         luniq     output records unique in the 1st PLIST file');
+	print('         ljoint    output records in the 1st PLIST present in the 2nd');
+	print('');
 	os.exit(1);
 end
 
 local cmd = arg[1];
 table.remove(arg, 1);
-local fp = io.xopen(arg[1]);
-if cmd == 'union' then bed_union(fp)
-elseif cmd == 'cov2' then bed_cov2(fp)
+if cmd == 'merge' then bed_merge(arg[1])
+elseif cmd == 'cov2' then bed_cov2(arg[1])
+elseif cmd == 'luniq' then plist_uniq(arg[1], arg[2])
+elseif cmd == 'ljoint' then plist_joint(arg[1], arg[2])
 else print('Unrecognized command.') end
