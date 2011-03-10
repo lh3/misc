0632a5b569a8327f5d4447bb3db1b614f1d1e8f3
Heng Li <lh3@live.co.uk>
2011-03-10 15:27:37 -0500

    probability to QQ plot

---
+++ crap/prob2qq.lua
@@ -0,0 +1,9 @@
+local a = {}
+for l in io.lines() do
+	a[#a+1] = tonumber(l);
+end
+table.sort(a);
+for i = 1, #a do
+	local x = (a[i] == 0 and 1e-20) or a[i];
+	print(-math.log10(i/#a), -math.log10(a[i]))
+end
